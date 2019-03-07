9db0b55765c566449d8f7601bad0f27f023ede21
Sondre Søberg <sonsobe@gmail.com>
2019-03-07 16:32:04 +0100

    Add a temporary express server, some scripts, and an update to README

---
+++ server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const buildDir = 'dist';
+const appDir = 'src';
+
+const indexFile = path.join(__dirname, buildDir, 'index.html');
+const isProd = process.env.NODE_ENV === 'production';
+
+app.use(express.static(path.join(__dirname, buildDir)));
+
+// Catch all error handler
+app.use((err, req, res, next) => {
+  if (!isProd) {
+    console.error(err);
+  }
+  res.status(500).send('500 - Internal Server Error');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(indexFile);
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log('Express server running at localhost:' + PORT);
+});
